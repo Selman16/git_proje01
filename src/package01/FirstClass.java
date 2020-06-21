7fda35a416351ceea980d97015dfbeb164c6c115
Süleyman <lor4916@gmail.com>
2020-06-21 22:14:32 +0300

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Ýllk commit from local repository");
+		System.out.println("ikinci commit from local repository");
 
 	}
 
